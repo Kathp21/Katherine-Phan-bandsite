5a0b98e36a71f296a30e07864f9de5eb78b481d7
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-01-04 23:49:25 -0800

    Create shows using DOM

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -17,12 +17,65 @@ const shows = [
 ]
 const gateway = document.querySelector(".shows");
 
-shows.forEach((show) => {
-    // console.log(show)
-    const date = document.createElement('h3')
-    const venue = document.createElement('h3')
-    const location = document.createElement('h3')
-    
-    gateway.appendChild(date);
-    console.log(date)
+const addToShows = (show) => {
+    const showEl = document.createElement('div')
+    showEl.classList.add('shows__details')
+
+    const showDate = document.createElement('p')
+    const showVenue = document.createElement('p')
+    const showLocation = document.createElement('p')
+    const showDateEl = document.createElement('p')
+    const showVenueEl = document.createElement('p')
+    const showLocationEl = document.createElement('p')
+
+    showDateEl.classList.add('shows__info')
+    showDateEl.classList.add('shows__info--bold')
+    showVenueEl.classList.add('shows__info')
+    showLocationEl.classList.add('shows__info')
+
+    showDate.innerText = "DATE"
+    showVenue.innerText = "VENUE"
+    showLocation.innerText = "LOCATION"
+    showDateEl.innerText = show.date
+    showVenueEl.innerText = show.venue
+    showLocationEl.innerText = show.location
+
+    showEl.appendChild(showDate)
+    showEl.appendChild(showDateEl)
+    showEl.appendChild(showVenue)
+    showEl.appendChild(showVenueEl)
+    showEl.appendChild(showLocation)
+    showEl.appendChild(showLocationEl)
+
+    gateway.appendChild(showEl)
+
+//Button
+    const btnContainer = document.createElement("div")
+    gateway.appendChild(btnContainer)
+
+    const btn = document.createElement("button")
+    btn.innerText = "BUY TICKETS"
+    btn.addEventListener('click', () => {
+        const clickButton = document.querySelector('.button')
+        console.log("hello")
+    })
+    gateway.appendChild(btn)
+
+    btnContainer.appendChild(btn)
+
+
+
+
+//Style button
+    btn.classList.add("shows__button")
+    btnContainer.classList.add("shows__btn-container")
+
+// Add Div for underline
+    const borderBottom = document.createElement('div')
+    borderBottom.classList.add('shows__divider')
+    gateway.appendChild(borderBottom)
+}
+
+shows.forEach ((show) => {
+    addToShows(show)
 })
