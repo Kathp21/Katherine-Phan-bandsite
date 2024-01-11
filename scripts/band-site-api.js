5b562e69b4ddfa369b706cc94c2bad36b8dad8b6
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-01-10 16:34:17 -0800

    Adds band site api and fixes some styling

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,20 +1,31 @@
-(async() => {
-    const newComment = {
-        userName:
-    }
-})
-
-class BandSiteApi {
+ export class BandSiteApi {
     constructor(apiKey) {
-        this.apiKey = apiKey
-        this.baseUrl = "https://project-1-api.herokuapp.com/"
+        this.apiKey = apiKey;
+        this.baseUrl = "https://project-1-api.herokuapp.com";
     }
 
-    async postComments(comment) {
-        const response = await axios.post(this.baseUrl), {
-            userName : 
-            date: 
-            comment: 
+    getComments = async () => {
+        try { 
+            const response = await axios.get(`${this.baseUrl}/comments?api_key=${this.apiKey}`)
+            return response.data
+        } catch(error) {
+            console.error(error.message)
         }
     }
 }
+
+const api = new BandSiteApi("a6b57c51-47cb-4915-9d1a-70b3cb940dfd")
+
+const displayComments = async () => {
+    const comments = await api.getComments()
+    comments.forEach(comment => {
+        console.log(comment) // debug
+
+    })
+}
+
+displayComments()
+
+// // const comments = api.getComments()
+// // console.log(comments)
+
