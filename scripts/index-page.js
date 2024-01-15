f5396e467f823da1ae2d51009d8dc5996f3ec93b
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-01-14 21:11:21 -0800

    Removes unused array

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,26 +1,3 @@
-// const previousComment = [
-//     {
-//         userName : "Connor Walton",
-//         date : "02/17/2021",
-//         content: "This is art. This is inexplicable magic expressed in the purest way, \
-//         everything that makes up this majestic work deserves reverence. \
-//         Let us appreciate this for what it is and what it contains."
-//     },
-//     {
-//         userName : "Emilie Beach",
-//         date : "01/09/2021",
-//         content: "I feel blessed to have seen them in person. What a show! They were just perfection. \
-//         If there was one day of my life I could relive, this would be it. What an incredible day."
-//     },
-//     {
-//         userName : "Miles Acosta",
-//         date : "12/20/2020",
-//         content: "I can't stop listening. Every time I hear one of their songs - the vocals - \
-//         it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity.\
-//          Can't get enough."
-//     }
-// ]
-
 import { BandSiteApi } from "./band-site-api.js";
 
 // Calling the constructor 
@@ -90,11 +67,13 @@ const createCommentElement = (comment) => {
 }
 
 const addCommentOnTopOfPage = (comment) => {
-    commentContainer.prepend(createCommentElement(comment))
+    const commentElement = createCommentElement(comment)
+    commentContainer.prepend(commentElement)    
 }
 
 const addCommentToPage = (comment) => {
-    commentContainer.appendChild(createCommentElement(comment))
+    const commentElement = createCommentElement(comment)
+    commentContainer.appendChild(commentElement)
 }
 
 function formatTimestamp(timestamp) {
